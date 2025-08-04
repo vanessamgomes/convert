2a09b7b71335d7ab7ee2a9ac789a3bb791cb2ab5
vanessamgomes <160276487+vanessamgomes@users.noreply.github.com>
2025-08-04 23:36:43 +0000

    validando o input

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("ta funcionando")
+const amount = document.getElementById("amount")
+
+//Manipulando o input amount para receber somente numeros
+amount.addEventListener("input", () => {
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+})
