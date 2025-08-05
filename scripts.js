227b203eaa82f69170742be7542d5c0cd62a9268
vanessamgomes <160276487+vanessamgomes@users.noreply.github.com>
2025-08-05 00:12:35 +0000

    exibindo o footer

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const JPY = 0.04
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 //Manipulando o input amount para receber somente numeros
 amount.addEventListener("input", () => {
@@ -18,4 +19,33 @@ amount.addEventListener("input", () => {
 //Captando o evento de submit do formulário
 form.onsubmit = (event) => {
     event.preventDefault()
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+            case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+        case "JPY":
+            convertCurrency(amount.value, JPY, "¥")
+            break
+}
+
+// Função para converter a moeda
+function convertCurrency(amount, price, symbol){
+    try{
+        // Aplica a classe que exibe o footer
+        footer.classList.add("show-result")
+    }catch (error) {
+        console.log(error)
+        
+        // Remove a classe que exibe o footer em caso de erro
+        footer.classList.remove("show-result")
+        alert("Erro ao converter a moeda. Tente novamente.")
+    }
+}
+
 }
